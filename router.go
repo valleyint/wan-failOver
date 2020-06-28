402bbca85652ef100466ff186cdce35cd0a6e1f0
darshan <darshan@melkote.com>
2020-06-28 18:31:34 +0530

    Moved hostname and password to file

--- router.go
+++ router.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -10,49 +13,47 @@ type wan struct {
 	name string
 }
 
-func Ping () bool {
-	pinger := exec.Command("fping" , "8.8.8.8")
+func Ping() bool {
+	pinger := exec.Command("fping", "8.8.8.8")
 	err := pinger.Run()
 	if err != nil {
-		log.Println("ping failed with result" , err)
+		log.Println("ping failed with result", err)
 		return false
 	} else {
 		return true
 	}
 }
 
-//considering wan 1 as start
-
-func main () {
+func main() {
 	Daemon()
 }
 
-func (w *wan) On () {
-	log.Println("switching on" , w.name)
-	cmd := exec.Command("ifup" , w.name)
+func (w *wan) On() {
+	log.Println("switching on", w.name)
+	cmd := exec.Command("ifup", w.name)
 	err := cmd.Run()
-	time.Sleep(5*time.Second)
+	time.Sleep(5 * time.Second)
 	if err != nil {
 		log.Println(err)
 	}
 	UpdateDNS()
 }
 
-func (w *wan) Off () {
-	log.Println("switching off" , w.name)
+func (w *wan) Off() {
+	log.Println("switching off", w.name)
 	cmd := exec.Command("ifdown", w.name)
 	err := cmd.Run()
-	time.Sleep(3*time.Second)
+	time.Sleep(3 * time.Second)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
-func (w *wan) Switch () {
-	log.Println("switching from" , w.name)
+func (w *wan) Switch() {
+	log.Println("switching from", w.name)
 	w.Off()
 	switch w.name {
-	case "eth1" :
+	case "eth1":
 		w.name = "eth2"
 	case "eth2":
 		w.name = "eth1"
@@ -60,21 +61,21 @@ func (w *wan) Switch () {
 		w.name = "eth1"
 	}
 	w.On()
-	log.Println("switched to" , w.name)
+	log.Println("switched to", w.name)
 }
 
-func InitWan () *wan {
+func InitWan() *wan {
 	log.Println("initializing wan")
 	wan := wan{name: "eth1"}
 	wan.On()
 	return &wan
 }
 
-func Daemon () {
+func Daemon() {
 	log.Println("deamon started")
 	wan := InitWan()
 	for {
-		time.Sleep(5 *time.Second)
+		time.Sleep(5 * time.Second)
 		err := Ping()
 		if err != true {
 			wan.Switch()
@@ -82,10 +83,29 @@ func Daemon () {
 	}
 }
 
+func DNSConfig() (host, password string) {
+	data, err := ioutil.ReadFile("/usr/local/etc/updatedns.conf")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	lines := strings.Split(string(data), "\n")
+	if len(lines) < 2 {
+		return
+	}
+	host = lines[0]
+	password = lines[1]
+	return
+}
 
-func UpdateDNS () {
+func UpdateDNS() {
+	host, password := DNSConfig()
+	if host == "" {
+		return
+	}
 	log.Println("updating dns")
-	com := exec.Command("curl", "-4" , "https://nypunya.lan.melkote.com:comgoogle@dyn.dns.he.net/nic/update?hostname=nypunya.lan.melkote.com")
+	url := fmt.Sprintf("https://dyn.dns.he.net/nic/update?hostname=%s&password=%s", host, password)
+	com := exec.Command("curl", "-4", url)
 	err := com.Run()
 	if err != nil {
 		log.Println(err)
