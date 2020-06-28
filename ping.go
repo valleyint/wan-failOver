4ec05ea9d2955257571e117fde0212497f1a8568
darshan <darshan@melkote.com>
2020-06-28 10:11:20 +0530

    merged onOff and ping , added a shell script to run on remote machine server

--- ping.go
+++ ping.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Ping () bool {
-	pinger := exec.Command("ping"  , "-c4" , "-W1500" , "8.8.8.8")
+	pinger := exec.Command("fping" , "8.8.8.8")
 	err := pinger.Run()
 	if err != nil {
 		fmt.Println("error is" , err)
@@ -19,3 +19,34 @@ func Ping () bool {
 func main () {
 	fmt.Println(Ping())
 }
+
+func On (portNo string) error {
+	cmd := exec.Command("ifup" , fmt.Sprintf("etc%v" , portNo))
+	err := cmd.Run()
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
+func Off (portNo string) error {
+	cmd := exec.Command("ifdown" , fmt.Sprintf("etc%v" , portNo))
+	err := cmd.Run()
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
+func Reboot () error {
+	cmd := exec.Command("reboot")
+	err := cmd.Run()
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
---
+++ push.sh
@@ -0,0 +1,2 @@
+scp ping.go pi@router.local:/home/pi/ping.go
+ssh pi@router.local go run ping.go
